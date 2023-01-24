faa60b98e738a2713ec624764b031dbfaef093bd
Rizal Purnawan <104172029+rizalpurnawan23@users.noreply.github.com>
2023-01-24 14:02:18 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,5 +4,10 @@ setup(
     name= 'email_reader',
     version= '0.0.1',
     description= 'A simple email reader module for my work.',
-    
+    url= 'git@github.com:rizalpurnawan23/email_reader.git',
+    author= 'Rizal Purnawan',
+    author_email= 'rizalpurnawan23@gmail.com',
+    license= 'not licensed',
+    packages= ['email_readers'],
+    zip_safe= False
 )
